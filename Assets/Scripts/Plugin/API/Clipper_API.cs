b102b2ec6c9143ea65a9c5e6f41a7a27055196b4
Alexander Watson <msnalexwatson@hotmail.co.uk>
2024-09-05 15:52:21 +0100

    Created interface
    Created an API for the lib to simplify things.
    
    - Some of the clipping functions (difference, union). Will require testing.

---
+++ Assets/Scripts/Plugin/API/Clipper_API.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Clipper2Lib;
+using Clipper2 = Clipper2Lib.Clipper;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
+
+namespace OnlyInvalid.Polygon.Clipper_API
+{
+    /// <summary>
+    /// This class is designed to integrate the clipper library with Unity data types.
+    /// </summary>
+    public static class Clipper
+    {
+        #region Intersection
+        public static IList<IList<Vector3>> Intersect(IEnumerable<Vector3> subject, IList<IList<Vector3>> clips, int precision = 7)
+        {
+            return Clipper2.Intersect(subject.ToPathsD(), clips.ToPathsD(), FillRule.NonZero, precision).ToPolygons();
+        }
+        public static IList<Vector3> Intersect(IEnumerable<Vector3> subject, IEnumerable<Vector3> clip, int precision = 7)
+        {
+            return Clipper2.Intersect(subject.ToPathsD(), clip.ToPathsD(), FillRule.NonZero, precision).ToPolygon();
+        }
+        #endregion
+
+        #region Union
+        public static IList<Vector3> Union(IEnumerable<Vector3> subject, IEnumerable<Vector3> clip, int precision = 7)
+        {
+            return Clipper2.Union(subject.ToPathsD(), clip.ToPathsD(), FillRule.NonZero, precision).ToPolygon();
+        }
+        public static IList<IList<Vector3>> Union(IEnumerable<Vector3> subject, IList<IList<Vector3>> clips, int precision = 7)
+        {
+            return Clipper2.Union(subject.ToPathsD(), clips.ToPathsD(), FillRule.NonZero, precision).ToPolygons();
+        }
+        #endregion
+
+        #region Difference
+        public static IList<Vector3> Difference(IEnumerable<Vector3> subject, IEnumerable<Vector3> clip, int precision = 7)
+        {
+            return Clipper2.Difference(subject.ToPathsD(), clip.ToPathsD(), FillRule.NonZero, precision).ToPolygon();
+        }
+        public static IList<IList<Vector3>> Difference(IEnumerable<Vector3> subject, IList<IList<Vector3>> clips, int precision = 7)
+        {
+            return Clipper2.Difference(subject.ToPathsD(), clips.ToPathsD(), FillRule.NonZero, precision).ToPolygons();
+        }
+        #endregion
+
+        #region Casting
+        private static PathD ToPathD(this IEnumerable<Vector3> polygon)
+        {
+            return new PathD(polygon.Select(point => point.ToPointD()));
+        }
+        private static PointD ToPointD(this Vector3 point)
+        {
+            double x = (double)point.x;
+            double y = (double)point.y;
+
+            return new PointD(x, y);
+        }
+        private static PathsD ToPathsD(this IEnumerable<Vector3> polygon)
+        {
+            return new PathsD()
+        {
+            ToPathD(polygon)
+        };
+        }
+        private static PathsD ToPathsD(this IList<IList<Vector3>> polygons)
+        {
+            PathsD result = new PathsD();
+
+            foreach (var polygon in polygons)
+            {
+                result.Add(ToPathD(polygon));
+            }
+
+            return result;
+        }
+        private static IList<Vector3> ToPolygon(this PathD path)
+        {
+            return path.Select(point => new Vector3((float)point.x, (float)point.y)).ToList();
+        }
+        private static IList<Vector3> ToPolygon(this PathsD path)
+        {
+            IList<Vector3> result = new List<Vector3>();
+
+            foreach (var subPath in path)
+            {
+                result.AddRange(subPath.ToPolygon());
+            }
+
+            return result;
+        }
+        private static IList<IList<Vector3>> ToPolygons(this PathsD path)
+        {
+            IList<IList<Vector3>> result = new List<IList<Vector3>>();
+
+            foreach (var subPath in path)
+            {
+                result.Add(subPath.ToPolygon());
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
